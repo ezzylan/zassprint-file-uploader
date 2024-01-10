55d1ee0c49969b667b66b7ef948bd1e8aa73322b
Ezlan Zulfiqree <66157842+ezzylan@users.noreply.github.com>
2024-01-10 13:17:57 +0800

    fix: extracted env variables to separate file

--- src/routes/dashboard/+page.js
+++ src/routes/dashboard/+page.js
@@ -1,11 +1,9 @@
 /** @type {import('./$types').PageLoad} */
 import { createClient } from "@supabase/supabase-js";
+import { PUBLIC_SUPABASE_URL, PUBLIC_SUPABASE_KEY } from "$env/static/public";
 
 export async function load() {
-	const supabase = createClient(
-		"https://pjritsgfzszporzzznep.supabase.co",
-		"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBqcml0c2dmenN6cG9yenp6bmVwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDQzODczMjAsImV4cCI6MjAxOTk2MzMyMH0.DUpSDdDN9Rlbx_4eF5PHp4DBebjSKnp1bZh4PNzzEgU"
-	);
+	const supabase = createClient(PUBLIC_SUPABASE_URL, PUBLIC_SUPABASE_KEY);
 
 	// const supabase = data.supabase
 	const { data, error } = await supabase.storage
