c3036a10da099ab9fc6d47e426e1156aeb46fad6
Ezlan Zulfiqree <66157842+ezzylan@users.noreply.github.com>
2024-01-14 22:22:23 +0800

    feat: add skeleton for better UX

--- src/routes/dashboard/+page.svelte
+++ src/routes/dashboard/+page.svelte
@@ -5,6 +5,7 @@
 	import * as Alert from "$lib/components/ui/alert";
 	import { Button } from "$lib/components/ui/button";
 	import * as Card from "$lib/components/ui/card";
+	import { Skeleton } from "$lib/components/ui/skeleton";
 	import { AlertCircle } from "lucide-svelte";
 	import { Reload } from "radix-icons-svelte";
 	import { onMount } from "svelte";
@@ -12,22 +13,23 @@
 
 	const supabase = data.supabase;
 	let files: any[];
-	let deletedFileNames: any[];
-	let disabled = false;
+	let deletedFileNames: string[];
+	let isRefreshing = true;
 
-	async function getFiles() {
+	function getFiles() {
 		files = [];
 		deletedFileNames = [];
 
-		const { data, error } = await supabase.storage
-			.from("files")
-			.list("", { sortBy: { column: "created_at", order: "desc" } });
+		setTimeout(async () => {
+			const { data, error } = await supabase.storage
+				.from("files")
+				.list("", { sortBy: { column: "created_at", order: "desc" } });
 
-		if (data) {
-			data.map((file) => {
-				files = [...files, file];
-			});
-		}
+			if (data) {
+				data.map((file) => (files = [...files, file]));
+			}
+			isRefreshing = false;
+		}, 1000);
 	}
 
 	async function openFile(filePath: string, download = false) {
@@ -43,7 +45,7 @@
 	}
 
 	async function checkFiles() {
-		disabled = true;
+		isRefreshing = true;
 
 		if (deletedFileNames.length > 0) {
 			const { data, error } = await supabase.storage
@@ -56,7 +58,6 @@
 		}
 
 		getFiles();
-		disabled = false;
 	}
 
 	onMount(getFiles);
@@ -68,8 +69,8 @@
 	>
 		Dashboard
 	</h2>
-	<Button {disabled} on:click={checkFiles}>
-		{#if disabled}
+	<Button disabled={isRefreshing} on:click={checkFiles}>
+		{#if isRefreshing}
 			<Reload class="mr-2 h-4 w-4 animate-spin" />
 			Refreshing...
 		{:else}
@@ -82,48 +83,107 @@
 	<AlertCircle class="h-4 w-4" />
 	<Alert.Title>Heads up!</Alert.Title>
 	<Alert.Description>
-		Refresh the page to check for new files.
+		Click the "Refresh" button to check for new files.
 	</Alert.Description>
 </Alert.Root>
 
-<div class="grid grid-cols-1 md:grid-cols-2 gap-4">
-	{#if files && files.length > 0}
-		{#each files as file}
-			<Card.Root>
-				<Card.Header>
-					<Card.Title class="truncate">{file.name}</Card.Title>
-				</Card.Header>
-				<Card.Content class="flex justify-between">
-					<div>
-						<Time timestamp={file.created_at} format="hh:mm:ss a" />
-						(<Time timestamp={file.created_at} relative />)
-					</div>
-					<Time timestamp={file.created_at} format="DD/MM/YYYY" />
-				</Card.Content>
-				<Card.Footer class="flex justify-between">
-					<Button
-						variant="destructive"
-						on:click={() => {
-							deletedFileNames = [...deletedFileNames, file.name];
-							files = files.filter((f) => f != file);
-						}}>Delete</Button
-					>
-					<div class="flex gap-2">
-						<Button on:click={() => openFile(file.name)}
-							>View</Button
-						>
+{#if isRefreshing}
+	<div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+		<Card.Root>
+			<Card.Header>
+				<Skeleton class="w-[300px] h-[20px]" />
+			</Card.Header>
+			<Card.Content class="flex justify-between">
+				<Skeleton class="w-[200px] h-[20px]" />
+				<Skeleton class="w-[80px] h-[20px]" />
+			</Card.Content>
+			<Card.Footer class="flex justify-between">
+				<Skeleton class="w-[75px] h-10 rounded-md" />
+				<div class="flex gap-2">
+					<Skeleton class="w-[65px] h-10 rounded-md" />
+					<Skeleton class="w-[100px] h-10 rounded-md" />
+				</div>
+			</Card.Footer>
+		</Card.Root>
+		<Card.Root>
+			<Card.Header>
+				<Skeleton class="w-[300px] h-[20px]" />
+			</Card.Header>
+			<Card.Content class="flex justify-between">
+				<Skeleton class="w-[200px] h-[20px]" />
+				<Skeleton class="w-[80px] h-[20px]" />
+			</Card.Content>
+			<Card.Footer class="flex justify-between">
+				<Skeleton class="w-[75px] h-10 rounded-md" />
+				<div class="flex gap-2">
+					<Skeleton class="w-[65px] h-10 rounded-md" />
+					<Skeleton class="w-[100px] h-10 rounded-md" />
+				</div>
+			</Card.Footer>
+		</Card.Root>
+		<Card.Root>
+			<Card.Header>
+				<Skeleton class="w-[300px] h-[20px]" />
+			</Card.Header>
+			<Card.Content class="flex justify-between">
+				<Skeleton class="w-[200px] h-[20px]" />
+				<Skeleton class="w-[80px] h-[20px]" />
+			</Card.Content>
+			<Card.Footer class="flex justify-between">
+				<Skeleton class="w-[75px] h-10 rounded-md" />
+				<div class="flex gap-2">
+					<Skeleton class="w-[65px] h-10 rounded-md" />
+					<Skeleton class="w-[100px] h-10 rounded-md" />
+				</div>
+			</Card.Footer>
+		</Card.Root>
+	</div>
+{:else}
+	<div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+		{#if files && files.length > 0}
+			{#each files as file}
+				<Card.Root>
+					<Card.Header>
+						<Card.Title class="truncate">{file.name}</Card.Title>
+					</Card.Header>
+					<Card.Content class="flex justify-between">
+						<div>
+							<Time
+								timestamp={file.created_at}
+								format="hh:mm:ss a"
+							/>
+							(<Time timestamp={file.created_at} relative />)
+						</div>
+						<Time timestamp={file.created_at} format="DD/MM/YYYY" />
+					</Card.Content>
+					<Card.Footer class="flex justify-between">
 						<Button
-							variant="secondary"
-							on:click={() => openFile(file.name, true)}
-							>Download</Button
+							variant="destructive"
+							on:click={() => {
+								deletedFileNames = [
+									...deletedFileNames,
+									file.name,
+								];
+								files = files.filter((f) => f != file);
+							}}>Delete</Button
 						>
-					</div>
-				</Card.Footer>
-			</Card.Root>
-		{/each}
-	{:else}
-		<h4 class="scroll-m-20 text-xl font-semibold tracking-tight">
-			No files available...
-		</h4>
-	{/if}
-</div>
+						<div class="flex gap-2">
+							<Button on:click={() => openFile(file.name)}
+								>View</Button
+							>
+							<Button
+								variant="secondary"
+								on:click={() => openFile(file.name, true)}
+								>Download</Button
+							>
+						</div>
+					</Card.Footer>
+				</Card.Root>
+			{/each}
+		{:else}
+			<h4 class="scroll-m-20 text-xl font-semibold tracking-tight">
+				No files available...
+			</h4>
+		{/if}
+	</div>
+{/if}
