23862c200d259fc33e5d10039f88fbf394b1aa66
Ezlan Zulfiqree <66157842+ezzylan@users.noreply.github.com>
2024-01-24 13:23:46 +0800

    feat: add footer to layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -6,11 +6,19 @@
 	<title>ZassPrint KPS</title>
 </svelte:head>
 
-<div class="container mx-auto p-8">
+<main class="container mx-auto p-8">
 	<h1
 		class="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl pb-8"
 	>
 		ZassPrint KPS
 	</h1>
 	<slot />
-</div>
+</main>
+
+<footer class="w-full bg-primary text-white py-4 fixed bottom-0">
+	<div class="container mx-auto px-4 md:px-6 lg:px-8">
+		<p class="leading-7 [&:not(:first-child)]:mt-6 text-center">
+			© 2024 ZassPrint. All rights reserved.
+		</p>
+	</div>
+</footer>
